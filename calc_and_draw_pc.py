16ba3f28c285eb6d583740531ffd3cf56679f13f
Blooden <pashpavel217@gmail.com>
2024-12-23 19:20:26 +0300

    sec

--- calc_and_draw_pc.py
+++ calc_and_draw_pc.py
@@ -1,4 +1,3 @@
-import os
 import matplotlib
 
 matplotlib.use('Agg')
@@ -12,29 +11,10 @@ class CalculationDrawServicePC:
     def __init__(self):
         pass
 
-    def calculate(self, data: dict) -> tuple:
-    # Расчет системы дифференциальных уравнений на заданном интервале
-        X = np.linspace(0, 50, 500)  # Временные значения от 0 до 50 (500 шагов)
-    
-    # Начальные условия: P0 = 1, остальные = 0
-        initial_conditions = [1] + [0] * 31  # 32 состояния
-        
-        # Проверяем входные данные
-        print("Initial conditions:", initial_conditions)
-        print("mu values:", data["mu"])
-        print("lat values:", data["lat"])
-
-        # Вызов odeint для решения системы уравнений
-        Y = odeint(
-            self.__describe_difference_equations,
-            initial_conditions,
-            X,
-            args=(data["mu"], data['lat'])
-        )
-
-        print("Shape of Y:", Y.shape)  # Проверяем размерность решения
+    def calculate(self, coefs: dict) -> dict:
+        # Инициализация полиномиальных функций и расчет Y и X
+        Y, X = self.__calculate(coefs)
         return X, Y
-
 
     def initFunctions(self, coefs):
         # Преобразование коэффициентов в объекты полиномиальных функций
@@ -44,120 +24,116 @@ class CalculationDrawServicePC:
             functions.append(polynomial)
         return functions
 
+    def __calculate(self, data: dict) -> tuple:
+        # Расчет системы дифференциальных уравнений на заданном интервале
+        X = np.linspace(0, 50, 500)  # Временные значения от 0 до 1
+        Y = None
+        niter = 500  # Количество итераций для расчетов
+
+        # Цикл по итерациям для решения дифференциальных уравнений
+        for i in range(niter):
+            Y = odeint(
+                self.__describe_difference_equations,
+                [1, 0, 0, 0, 0, 0, 0, 0],
+                X,
+                (data["mu"],
+                 data['lat'])
+            )
+            # Нормализация результатов, чтобы они были в пределах [0, 1]
+            for normalize_row in range(len(Y)):
+                for normalize_column in range(len(Y[normalize_row])):
+                    if Y[normalize_row][normalize_column] < 0.0:
+                        Y[normalize_row][normalize_column] = 0.0
+        return Y, X
+
     def __describe_difference_equations(self, u, t, mus, lat):
         # Распаковка переменных состояния
-        (P0, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15,
-        P16, P17, P18, P19, P20, P21, P22, P23, P24, P25, P26, P27, P28, P29, P30, P31) = u
+        [P0_t, P1_t, P2_t, P3_t, P4_t, P5_t, P6_t, P7_t] = u
+        mus1, mus2, mus3 = mus
 
-        mus1, mus2, mus3, mus4, mus5 = mus  # Интенсивности восстановления
-        lat1, lat2, lat3, lat4, lat5 = lat  # Интенсивности отказов
+        lat1, lat2, lat3 = lat
+        dL0_dx = - (lat1 + lat2 + lat3) * P0_t + mus1 * P1_t + mus2 * P2_t + mus3 * P3_t
 
-        # Базовое состояние (все ПК исправны)
-        dP0_dt = -(lat1 + lat2 + lat3 + lat4 + lat5) * P0 + mus1 * P1 + mus2 * P2 + mus3 * P4 + mus4 * P8 + mus5 * P16
+        dL1_dx = lat1 * P0_t - (lat2 + lat3 + mus1) * P1_t + mus2 * P4_t + mus3 * P5_t
 
-        # Одиночные неисправности
-        dP1_dt = lat1 * P0 - (mus1 + lat2 + lat3 + lat4 + lat5) * P1 + mus2 * P3 + mus3 * P5 + mus4 * P9 + mus5 * P17
-        dP2_dt = lat2 * P0 - (mus2 + lat1 + lat3 + lat4 + lat5) * P2 + mus1 * P3 + mus3 * P6 + mus4 * P10 + mus5 * P18
-        dP4_dt = lat3 * P0 - (mus3 + lat1 + lat2 + lat4 + lat5) * P4 + mus1 * P5 + mus2 * P6 + mus4 * P12 + mus5 * P20
-        dP8_dt = lat4 * P0 - (mus4 + lat1 + lat2 + lat3 + lat5) * P8 + mus1 * P9 + mus2 * P10 + mus3 * P12 + mus5 * P24
-        dP16_dt = lat5 * P0 - (mus5 + lat1 + lat2 + lat3 + lat4) * P16 + mus1 * P17 + mus2 * P18 + mus3 * P20 + mus4 * P24
+        dL2_dx = lat2 * P0_t - (lat1 + lat3 + mus2) * P2_t + mus1 * P4_t + mus3 * P6_t
 
-        # Двойные неисправности
-        dP3_dt = lat1 * P2 + lat2 * P1 - (mus1 + mus2 + lat3 + lat4 + lat5) * P3 + mus3 * P7 + mus4 * P11 + mus5 * P19
-        dP5_dt = lat1 * P4 + lat3 * P1 - (mus1 + mus3 + lat2 + lat4 + lat5) * P5 + mus2 * P7 + mus4 * P13 + mus5 * P21
-        dP6_dt = lat2 * P4 + lat3 * P2 - (mus2 + mus3 + lat1 + lat4 + lat5) * P6 + mus1 * P7 + mus4 * P14 + mus5 * P22
-        dP9_dt = lat1 * P8 + lat4 * P1 - (mus1 + mus4 + lat2 + lat3 + lat5) * P9 + mus2 * P11 + mus3 * P13 + mus5 * P25
-        dP10_dt = lat2 * P8 + lat4 * P2 - (mus2 + mus4 + lat1 + lat3 + lat5) * P10 + mus1 * P11 + mus3 * P14 + mus5 * P26
-        dP11_dt = lat1 * P10 + lat2 * P9 + lat4 * P3 - (mus1 + mus2 + mus4 + lat3 + lat5) * P11
-        dP12_dt = lat3 * P8 + lat4 * P4 - (mus3 + mus4 + lat1 + lat2 + lat5) * P12 + mus1 * P13 + mus2 * P14 + mus5 * P28
-        dP13_dt = lat1 * P12 + lat3 * P9 + lat4 * P5 - (mus1 + mus3 + mus4 + lat2 + lat5) * P13
-        dP14_dt = lat2 * P12 + lat3 * P10 + lat4 * P6 - (mus2 + mus3 + mus4 + lat1 + lat5) * P14
-        dP15_dt = lat1 * P14 + lat2 * P13 + lat3 * P11 + lat4 * P7 - (mus1 + mus2 + mus3 + mus4 + lat5) * P15
-        dP17_dt = lat1 * P16 + lat5 * P1 - (mus1 + mus5 + lat2 + lat3 + lat4) * P17 + mus2 * P19 + mus3 * P21 + mus4 * P25
-        dP18_dt = lat2 * P16 + lat5 * P2 - (mus2 + mus5 + lat1 + lat3 + lat4) * P18 + mus1 * P19 + mus3 * P22 + mus4 * P26
-        dP19_dt = lat1 * P18 + lat2 * P17 + lat5 * P3 - (mus1 + mus2 + mus5 + lat3 + lat4) * P19
-        dP20_dt = lat3 * P16 + lat5 * P4 - (mus3 + mus5 + lat1 + lat2 + lat4) * P20 + mus1 * P21 + mus2 * P22 + mus4 * P28
-        dP21_dt = lat1 * P20 + lat3 * P17 + lat5 * P5 - (mus1 + mus3 + mus5 + lat2 + lat4) * P21
-        dP22_dt = lat2 * P20 + lat3 * P18 + lat5 * P6 - (mus2 + mus3 + mus5 + lat1 + lat4) * P22
-        dP23_dt = lat1 * P22 + lat2 * P21 + lat3 * P19 + lat5 * P7 - (mus1 + mus2 + mus3 + mus5 + lat4) * P23
-        dP24_dt = lat4 * P16 + lat5 * P8 - (mus4 + mus5 + lat1 + lat2 + lat3) * P24 + mus1 * P25 + mus2 * P26 + mus3 * P28
-        dP25_dt = lat1 * P24 + lat4 * P17 + lat5 * P9 - (mus1 + mus4 + mus5 + lat2 + lat3) * P25
-        dP26_dt = lat2 * P24 + lat4 * P18 + lat5 * P10 - (mus2 + mus4 + mus5 + lat1 + lat3) * P26
-        dP27_dt = lat1 * P26 + lat2 * P25 + lat4 * P19 + lat5 * P11 - (mus1 + mus2 + mus4 + mus5 + lat3) * P27
-        dP28_dt = lat3 * P24 + lat4 * P20 + lat5 * P12 - (mus3 + mus4 + mus5 + lat1 + lat2) * P28
-        dP29_dt = lat1 * P28 + lat3 * P25 + lat4 * P21 + lat5 * P13 - (mus1 + mus3 + mus4 + mus5 + lat2) * P29
-        dP30_dt = lat2 * P28 + lat3 * P26 + lat4 * P22 + lat5 * P14 - (mus2 + mus3 + mus4 + mus5 + lat1) * P30
+        dL3_dx = lat3 * P0_t - (lat1 + lat2 + mus3) * P3_t + mus1 * P5_t + mus2 * P6_t
 
-        # Тройные и больше (пример — аналогичный подход можно расширить)
-        dP7_dt = lat1 * P6 + lat2 * P5 + lat3 * P3 - (mus1 + mus2 + mus3 + lat4 + lat5) * P7
-        dP31_dt = lat1 * P30 + lat2 * P29 + lat3 * P27 + lat4 * P23 + lat5 * P15 - (mus1 + mus2 + mus3 + mus4 + mus5) * P31
+        dL4_dx = lat2 * P1_t + lat1 * P2_t - (lat3 + mus1 + mus2) * P4_t + mus3 * P7_t
 
-        # Возвращаем производные всех состояний
-        return [
-            dP0_dt, dP1_dt, dP2_dt, dP3_dt, dP4_dt, dP5_dt, dP6_dt, dP7_dt,
-            dP8_dt, dP9_dt, dP10_dt, dP11_dt, dP12_dt, dP13_dt, dP14_dt, dP15_dt,
-            dP16_dt, dP17_dt, dP18_dt, dP19_dt, dP20_dt, dP21_dt, dP22_dt, dP23_dt,
-            dP24_dt, dP25_dt, dP26_dt, dP27_dt, dP28_dt, dP29_dt, dP30_dt, dP31_dt
-        ]
+        dL5_dx = lat3 * P1_t + lat1 * P3_t - (lat2 + mus1 + mus3) * P5_t + mus2 * P7_t
+
+        dL6_dx = lat3 * P2_t + lat2 * P3_t - (lat1 + mus2 + mus3) * P6_t + mus1 * P7_t
+
+        dL7_dx = lat3 * P4_t + lat2 * P5_t + lat1 * P6_t - (mus1 + mus2 + mus3) * P7_t
+
+        # Возвращение вычисленных производных
+        return [dL0_dx, dL1_dx, dL2_dx, dL3_dx, dL4_dx, dL5_dx, dL6_dx, dL7_dx]
 
     def save_plots(self, Y: list):
         fig1, ax1 = plt.subplots(figsize=(16, 8))
-        time_intervals = np.linspace(0, 50, len(Y))  # X имеет 500 значений
+        time_intervals = np.linspace(0, 50, 500)
 
-        # Распаковка всех 32 состояний
-        states = Y.T  # Теперь states содержит 32 строки (P0, P1, ..., P31)
+        P0, P1, P2, P3, P4, P5, P6, P7 = Y.T
 
-        # Генерация подписей для состояний (битовые комбинации для 5 ПК)
-        labels = ["Базовое состояние"]  # P0
-        for i in range(1, 32):
-            failed_pcs = ", ".join([f"ПК {j+1}" for j in range(5) if (i >> j) & 1])
-            labels.append(f"Неисправность: {failed_pcs}")
+        # Определяем все линии для удобства
+        lines = [
+            (P0, 'Базовое состояние'), (P1, 'Неисправность первого ПК'),
+            (P2, 'Неисправность второго ПК'), (P3, 'Неисправность третьего ПК'),
+            (P4, 'Неисправность 1 и 2 ПК'), (P5, 'Неисправность 1 и 3 ПК'),
+            (P6, 'Неисправность 2 и 3 ПК'), (P7, 'Неисправность всех компьютеров'),
+        ]
 
-        # Добавление всех линий на график
-        for i, state in enumerate(states):
-            ax1.plot(time_intervals, state, label=labels[i])
+        # Строим линии графика
+        for L, label in lines:
+            ax1.plot(time_intervals, L, label=label)
 
         # Настройка графика
         ax1.set_xlabel('Время, часы')
         ax1.set_ylabel('Вероятность')
-        ax1.set_title('Вероятность нахождения системы в каждом состоянии')
+        ax1.set_title('Вероятностного нахождения системы в состоянии')
         ax1.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
         plt.tight_layout()
 
-        # Сохранение первого графика
-        images_dir = "images"
-        os.makedirs(images_dir, exist_ok=True)  # Создаем папку, если её нет
-        path1 = os.path.join(images_dir, "petal_plot_pc.png")
-        plt.savefig(path1)
+        # Сохранение первого графика в буфер
+        plt.savefig("images/" + "petal_plot_pc.png")
 
-        # Построение увеличенного графика для первых 10 часов
+        # Визуализация увеличенного графика
         fig2, ax2 = plt.subplots(figsize=(16, 8))
+
+        # Обрезаем временные интервалы (например, первые 10 единиц времени)
         zoomed_intervals = time_intervals[time_intervals <= 10]
         zoomed_solution = Y[:len(zoomed_intervals)]
 
-        for i, state in enumerate(zoomed_solution.T):
-            ax2.plot(zoomed_intervals, state, label=labels[i])
+        # Разделение обрезанного решения
+        P0_zoomed, P1_zoomed, P2_zoomed, P3_zoomed, P4_zoomed, P5_zoomed, P6_zoomed, P7_zoomed = zoomed_solution.T
+
+        # Строим линии для увеличенного графика
+        zoomed_lines = [
+            (P0_zoomed, 'Базовое состояние'), (P1_zoomed, 'Неисправность первого ПК'),
+            (P2_zoomed, 'Неисправность второго ПК'), (P3_zoomed, 'Неисправность третьего ПК'),
+            (P4_zoomed, 'Неисправность 1 и 2 ПК'), (P5_zoomed, 'Неисправность 1 и 3 ПК'),
+            (P6_zoomed, 'Неисправность 2 и 3 ПК'), (P7_zoomed, 'Неисправность всех компьютеров'),
+        ]
+
+        for L, label in zoomed_lines:
+            ax2.plot(zoomed_intervals, L, label=label)
 
         # Настройка увеличенного графика
         ax2.set_xlabel('Время (увеличенный масштаб), часы')
         ax2.set_ylabel('Вероятность')
-        ax2.set_title('Увеличенный график вероятностей состояний')
+        ax2.set_title('Вероятностного нахождения системы в состоянии (увеличенный масштаб)')
+        ax2.set_ylim(0, 0.5)
         ax2.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
         plt.tight_layout()
 
-        # Сохранение второго графика
-        path2 = os.path.join(images_dir, "petal_plot_pc_zoom.png")
-        plt.savefig(path2)
+        # Сохранение второго графика в буфер
+        plt.savefig("images/" + "petal_plot_pc_zoom.png")
 
         plt.clf()  # Очистка текущей фигуры
 
-        print(f"Saving plots to: {path1}, {path2}")  # Логирование путей
-        print("Plots saved successfully!")
-
         return {
-            "image1": path1,
-            "image2": path2
+            "image1": None,  # Полный график
+            "image2": None  # Увеличенный график
         }
-
-
-
